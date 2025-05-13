c611b804b7b597b0b1a64cb988e8b110d90f5e19
Shijan-Pias <shijanpias3@gmail.com>
2025-05-13 23:03:49 +0600

    Added login form and fixed navbar

--- src/Components/homelayout/SocialLogin.jsx
+++ src/Components/homelayout/SocialLogin.jsx
@@ -4,7 +4,7 @@ import { FaGoogle,FaGithub } from "react-icons/fa6";
 const SocialLogin = () => {
     return (
         <div>
-            <h1 className='font-bold mb-5'>Login With</h1>
+            <h1 className='font-bold mb-5 text-black'>Login With</h1>
             <div className=''>
                 <button className='btn btn-outline btn-primary mb-3 w-full'> <FaGoogle size={20}/>Login With Google</button>
                 <button className='btn btn-outline btn-secondary w-full '><FaGithub size={20} /> Login With Github</button>
